e38d1a9c6f36f206b2a3885533e5590409164682
Ivan Karmanov <devkarmanov@gmail.com>
2025-09-11 12:35:21 +0300

    #14 User info endpoint
    - Added a way to get user information
    - Shared classes have been moved to a separate module

---
+++ auth-service/src/main/java/van/karm/auth/application/service/auth/AuthService.java
@@ -0,0 +1,8 @@
+package van.karm.auth.application.service.auth;
+
+import van.karm.auth.presentation.dto.response.token.Tokens;
+
+public interface AuthService {
+    Tokens login(String username, String password, String deviceId);
+    void register(String username, String password);
+}
